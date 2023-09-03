95c39b207413e0bd5ee76448995e137230ccccfa
Ricardo Cardoso <ricardo.estudos@hotmail.com>
2023-09-03 17:11:20 -0300

    personalização do original

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Trilha JS Developer - Pokedex
+# Pokedex + API (DIO)
+
+Em processo de criação
+Tecnologias utilizadas:
+
+HTML5
+CSS3
+JAVASCRIPT
+
+# ScreenShot
+![image](https://github.com/cardosokks/pokedex_dio/assets/106789759/b72a73d7-eebe-400a-9c39-92d144fe0617)
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,63 @@
-const pokemonList = document.getElementById('pokemonList')
-const loadMoreButton = document.getElementById('loadMoreButton')
+const pokemonListOl = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('LoadMoreButton')
+maxRecords = 649; // Quantidade de pokemons que aparecerão no total;
+const limit = 25; // Quantidade de pokemons que aparecerão á cada load;
+let offset = 0; // Inicio
 
-const maxRecords = 151
-const limit = 10
-let offset = 0;
+function traduction(string) { // Essa função irá traduzir os tipos de pokemons para pt/br
+    if (string == "normal") { string = "Normal"}
+    else if (string == "grass") { string = "Folha"}
+    else if (string == "poison") {string = "Veneno"}
+    else if (string == "fire") {string = "Fogo"}
+    else if (string == "bug") {string = "Inseto"}
+    else if (string == "water") {string = "Água"}
+    else if (string == "flying") {string = "Voador"}
+    else if (string == "electric") {string = "Elétrico"}
+    else if (string == "ground") {string = "Subsolo"}
+    else if (string == "fighting") {string = "Lutador"}
+    else if (string == "rock") {string = "Pedra"}
+    else if (string == "psychic") {string = "Psíquico"}
+    else if (string == "steel") {string = "Ferro"}
+    else if (string == "ice") {string = "Gelo"}
+    else if (string == "dragon") {string = "Dragão"}
+    else if (string == "dark") {string = "Escuridão"}
+    else if (string == "ghost") {string = "Fantasma"}
+    else if (string == "fairy") {string = "Fada"}
+    return string
 
-function convertPokemonToLi(pokemon) {
-    return `
+}
+
+function loadPokemonItens(offset, limit){
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) => `
         <li class="pokemon ${pokemon.type}">
             <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
-
+            <span class="name">${traduction(pokemon.name)}</span>
             <div class="detail">
                 <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${traduction(type)}</li>`).join("")}
                 </ol>
-
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
             </div>
         </li>
-    `
-}
-
-function loadPokemonItens(offset, limit) {
-    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
-        pokemonList.innerHTML += newHtml
+    `).join('')
+        pokemonListOl.innerHTML += newHtml 
     })
+    
 }
 
-loadPokemonItens(offset, limit)
+loadPokemonItens(offset, limit);
 
 loadMoreButton.addEventListener('click', () => {
-    offset += limit
-    const qtdRecordsWithNexPage = offset + limit
+    offset += limit;
+    const qtdRecordNextPage = offset + limit
 
-    if (qtdRecordsWithNexPage >= maxRecords) {
-        const newLimit = maxRecords - offset
-        loadPokemonItens(offset, newLimit)
+    if(qtdRecordNextPage >= maxRecords){
+            const newLimit = maxRecords - offset
+            loadPokemonItens(offset, newLimit);
 
-        loadMoreButton.parentElement.removeChild(loadMoreButton)
+            loadMoreButton.parentElement.removeChild(loadMoreButton);
     } else {
-        loadPokemonItens(offset, limit)
+        loadPokemonItens(offset, limit);
     }
 })
